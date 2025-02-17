798d874b1a52a5dd226251c4aa3cffdba16e3ee1
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-18 00:27:01 +0900

    Exclude Certain Folders for Telescope Searching

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,6 +13,13 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "obj",
+            "bin"
+          }
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {}
